3f896306c8dbed76cb5aa1aea34f918b06bdbad5
chenbaifu <1503123330@qq.com>
2022-06-11 10:22:23 +0800

    注册登陆已上传

--- src/main/java/com/example/combinedkeywords/controller/UserController.java
+++ src/main/java/com/example/combinedkeywords/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.combinedkeywords.controller;
 
 import com.example.combinedkeywords.service.UserService;
 import com.example.combinedkeywords.util.JsonResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -13,11 +15,13 @@ import javax.annotation.Resource;
 public class UserController {
     @Resource
     private UserService userService;
+    @PutMapping("register")
     public JsonResult register(String name,String password){
         return userService.register(name,password);
     }
+    @PostMapping("login")
     public JsonResult login(String name,String password){
-        return login(name, password);
+        return userService.login(name, password);
     }
 
 }
